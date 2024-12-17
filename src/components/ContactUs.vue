fd9a9202d4154d5c871bcae4edcaab09949f3e62
Manish Dhuri <manishdhuri2000@gmail.com>
2024-12-17 18:31:45 +0530

    Updated contact us page UI

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="contact-us-banner">
     <div class="row justify-center q-py-xl">
-      <div class="col-4 shadow-2">
+      <div class="col-xs-10 col-md-4 shadow-2">
         <div class="contact-form q-pa-md">
           <h4 class="text-center">Connect with us</h4>
           <div class="q-gutter-md">
-            <q-form @submit="handleSubmit" class="q-gutter-md">
-              <q-input v-model="name" label="Name" required autofocus filled />
-              <q-input v-model="email" label="Email" type="email" required filled />
-              <q-input v-model="message" label="Message" type="textarea" rows="5" required filled />
+            <q-form class="q-gutter-md">
+
+              <q-input v-model="name" label="Name" stack-label autofocus lazy-rules :dense="dense"
+                :rules="[val => val && val.length > 0 || 'Please type something']" />
+              <q-input v-model="email" label="Email" stack-label type="email" lazy-rules
+                :rules="[val => val && val.length > 0 || 'Please type something']" />
+              <q-input v-model="message" label="Message" stack-label type="textarea" rows="5" />
               <q-btn type="submit" label="Submit" color="primary" />
             </q-form>
           </div>
